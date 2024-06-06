d7c468316c86bfe5328bd1df11f160379c0a9e52
Slimseto <147414784+Slimseto@users.noreply.github.com>
2024-06-06 14:25:28 +0800

    enhanced projects session and added links and information

--- src/ProjectsSection.js
+++ src/ProjectsSection.js
@@ -15,8 +15,35 @@ const ProjectsSection = () => {
               </p>
             </div>
     
-            {/* Project 2 */}
-            {/* Add descriptions for other projects as well */}
+
+            {/* Repeat the structure for each project */}
+            <div className="mb-8">
+              <h3 className="text-xl font-semibold text-blue-700">Spam Mail Classification</h3>
+              <p className="text-gray-700 mt-2">
+                Classifying spam mails using machine learning models to enhance email filtering, with exploratory data analysis done in Jupyter Notebook.
+              </p>
+              <a href="https://github.com/thetruthkenhurt/Spam-mail-classification" className="text-blue-500 hover:text-blue-800">View Repository</a>
+            </div>
+
+            {/* Climate Research Project */}
+            <div className="mb-8">
+              <h3 className="text-xl font-semibold text-blue-700">Climate Research</h3>
+              <p className="text-gray-700 mt-2">
+                Analyzing trends in climate change in Singapore using Python and machine learning algorithms.
+                This project aims to provide insights into climate patterns and contribute to environmental research.
+              </p>
+              <a href="https://github.com/thetruthkenhurt/climate-research" className="text-blue-500 hover:text-blue-800">View Repository</a>
+            </div>
+
+            {/* Runescape Automation Scripts Project */}
+            <div className="mb-8">
+              <h3 className="text-xl font-semibold text-blue-700">Runescape Automation Scripts</h3>
+              <p className="text-gray-700 mt-2">
+                Python Scripts for automating tasks in Old School Runescape (OSRS) for self-learning purposes.
+                Exploring automation in gaming environments to enhance understanding of scripting and AI applications of computer vision using libraries like OpenCV.
+              </p>
+              <a href="https://github.com/thetruthkenhurt/runescape-automation-scripts" className="text-blue-500 hover:text-blue-800">View, Repository</a>
+            </div>
           </div>
         </section>
   );
